1e706710df9aa756e66a898772b9e2abd040178c
Nicola Vigano <nicola.vigano@esrf.fr>
2020-04-10 18:16:36 +0200

    More tests
    Signed-off-by: Nicola Vigano <nicola.vigano@esrf.fr>

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -71,32 +71,43 @@ class TestSolvers(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
-        test_vols_shape = (10, 10, 5)
-        self.vol_rand_2d = np.fmin(np.random.rand(*test_vols_shape[:2]) + eps, 1)
-        self.proj_matrix_rand_2d = np.random.rand(5 * self.vol_rand_2d.size, self.vol_rand_2d.size)
-        self.data_rand_2d = self.fwd_op(self.proj_matrix_rand_2d, self.vol_rand_2d)
+        self.test_vols_shape = (10, 10, 5)
 
-        self.vol_flat_2d = utils_proc.get_circular_mask(test_vols_shape[:2], -2)
-        self.proj_matrix_flat_2d = np.random.rand(2 * self.vol_flat_2d.size, self.vol_flat_2d.size)
-        self.data_flat_2d = self.fwd_op(self.proj_matrix_flat_2d, self.vol_flat_2d)
-        self.data_flat_2d += (2 * np.random.rand(*self.data_flat_2d.shape) - 1) * 1e-3
+        self.vol_rand_2d = np.fmin(np.random.rand(*self.test_vols_shape[:2]) + eps, 1)
+        self.vol_flat_2d = utils_proc.get_circular_mask(self.test_vols_shape[:2], -2)
+
+        self.proj_matrix_2d = (np.random.rand(5 * self.vol_rand_2d.size, self.vol_rand_2d.size) > 0.5).astype(np.float32)
+
+        self.data_rand_2d = self.fwd_op(self.proj_matrix_2d, self.vol_rand_2d)
+
+        self.data_flat_2d = self.fwd_op(self.proj_matrix_2d, self.vol_flat_2d)
+        self.data_flat_2d += np.random.randn(*self.data_flat_2d.shape) * 1e-3
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
     def fwd_op(self, M, x):
         return np.dot(M, x.flatten())
+
     def bwd_op(self, M, y, x_shape):
         return np.reshape(np.dot(y.flatten(), M), x_shape)
+
+    def get_A_At(self, vol_dims):
+        if vol_dims.lower() == '2d':
+            def A(x):
+                return self.fwd_op(self.proj_matrix_2d, x)
+
+            def At(y):
+                return self.bwd_op(self.proj_matrix_2d, y, self.test_vols_shape[:2])
+
+        else:
+            raise ValueError('Only 2D implemented.')
+        return (A, At)
 
     def test_000_SIRT(self):
         """Test SIRT algorithm in 2D."""
 
-        def A(x):
-            return self.fwd_op(self.proj_matrix_rand_2d, x)
-        def At(y):
-            return self.bwd_op(self.proj_matrix_rand_2d, y, self.vol_rand_2d.shape)
-
+        (A, At) = self.get_A_At('2d')
         algo = solvers.Sirt()
         (sol, residual) = algo(A, self.data_rand_2d, 2500, At=At)
 
@@ -107,11 +118,7 @@ class TestSolvers(unittest.TestCase):
     def test_001_CPLS(self):
         """Test Chambolle-Pock least-squares algorithm in 2D."""
 
-        def A(x):
-            return self.fwd_op(self.proj_matrix_rand_2d, x)
-        def At(y):
-            return self.bwd_op(self.proj_matrix_rand_2d, y, self.vol_rand_2d.shape)
-
+        (A, At) = self.get_A_At('2d')
         algo = solvers.CP()
         (sol, residual) = algo(A, self.data_rand_2d, 2000, At=At)
 
@@ -122,12 +129,8 @@ class TestSolvers(unittest.TestCase):
     def test_002_SIRTTV(self):
         """Test SIRT TV-min algorithm in 2D."""
 
-        def A(x):
-            return self.fwd_op(self.proj_matrix_flat_2d, x)
-        def At(y):
-            return self.bwd_op(self.proj_matrix_flat_2d, y, self.vol_flat_2d.shape)
-
-        reg = solvers.Regularizer_TV2D(1e-3)
+        (A, At) = self.get_A_At('2d')
+        reg = solvers.Regularizer_TV2D(1e-4)
         algo = solvers.CP(regularizer=reg)
         (sol, residual) = algo(A, self.data_flat_2d, 2500, At=At)
 
@@ -135,15 +138,11 @@ class TestSolvers(unittest.TestCase):
               end='', flush=True)
         assert np.all(np.isclose(sol, self.vol_flat_2d, atol=1e-3))
 
-    def test_003_CPLSTV(self):
-        """Test Chambolle-Pock least-squares TV-min algorithm in 2D."""
+    def test_003_CPLSTV_unconstrained(self):
+        """Test Chambolle-Pock unconstrained least-squares TV-min algorithm in 2D."""
 
-        def A(x):
-            return self.fwd_op(self.proj_matrix_flat_2d, x)
-        def At(y):
-            return self.bwd_op(self.proj_matrix_flat_2d, y, self.vol_flat_2d.shape)
-
-        reg = solvers.Regularizer_TV2D(1e-3)
+        (A, At) = self.get_A_At('2d')
+        reg = solvers.Regularizer_TV2D(1e-4)
         algo = solvers.CP(regularizer=reg)
         (sol, residual) = algo(A, self.data_flat_2d, 2500, At=At)
 
@@ -151,4 +150,26 @@ class TestSolvers(unittest.TestCase):
               end='', flush=True)
         assert np.all(np.isclose(sol, self.vol_flat_2d, atol=1e-3))
 
+    def test_004_CPLSTV_constrained01(self):
+        """Test Chambolle-Pock constrained [0, 1] least-squares TV-min algorithm in 2D."""
 
+        (A, At) = self.get_A_At('2d')
+        reg = solvers.Regularizer_TV2D(1e-4)
+        algo = solvers.CP(regularizer=reg)
+        (sol, residual) = algo(A, self.data_flat_2d, 2500, At=At, lower_limit=0, upper_limit=1)
+
+        print('Max absolute deviation is: {}. '.format(np.max(np.abs(self.vol_flat_2d - sol))),
+              end='', flush=True)
+        assert np.all(np.isclose(sol, self.vol_flat_2d, atol=1e-3))
+
+    def test_005_CPLSTV_unconstrained_precond(self):
+        """Test Chambolle-Pock preconditioned, unconstrained least-squares TV-min algorithm in 2D."""
+
+        (A, At) = self.get_A_At('2d')
+        reg = solvers.Regularizer_TV2D(1e-4)
+        algo = solvers.CP(regularizer=reg)
+        (sol, residual) = algo(A, self.data_flat_2d, 2500, At=At, precondition=True)
+
+        print('Max absolute deviation is: {}. '.format(np.max(np.abs(self.vol_flat_2d - sol))),
+              end='', flush=True)
+        assert np.all(np.isclose(sol, self.vol_flat_2d, atol=1e-3))
