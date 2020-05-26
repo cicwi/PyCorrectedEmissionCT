0bf73ac83640b906f8e40243e2854221d8dcf7a7
Nicola Vigano <nicola.vigano@esrf.fr>
2020-05-26 17:19:32 +0200

    Utils: multi-dimension mask creation
    Signed-off-by: Nicola Vigano <nicola.vigano@esrf.fr>

--- corrct/utils_proc.py
+++ corrct/utils_proc.py
@@ -12,30 +12,47 @@ from . import operators
 from . import solvers
 
 
-def get_circular_mask(vol_shape_yx, radius_offset=0, mask_drop_off='const', data_type=np.float32):
+def get_circular_mask(
+        vol_shape, radius_offset=0, coords_ball=None, mask_drop_off='const', data_type=np.float32):
     """Computes a circular mask for the reconstruction volume.
 
-    :param vol_shape_yx: The size of the volume.
-    :type vol_shape_yx: numpy.array_like
+    :param vol_shape: The size of the volume.
+    :type vol_shape: numpy.array_like
     :param radius_offset: The offset with respect to the volume edge.
     :type radius_offset: float. Optional, default: 0
+    :param coords_ball: The coordinates to consider for the non-masked region.
+    :type coords_ball: list of dimensions. Optional, default: None
     :param data_type: The mask data type.
     :type data_type: numpy.dtype. Optional, default: np.float32
 
     :returns: The circular mask.
     :rtype: (numpy.array_like)
     """
-    yy = np.linspace(-(vol_shape_yx[0]-1)/2, (vol_shape_yx[0]-1)/2, vol_shape_yx[0], dtype=data_type)
-    xx = np.linspace(-(vol_shape_yx[1]-1)/2, (vol_shape_yx[1]-1)/2, vol_shape_yx[1], dtype=data_type)
-    (yy, xx) = np.meshgrid(yy, xx, indexing='ij')
+    vol_shape = np.array(vol_shape, dtype=np.int)
 
-    radius = (vol_shape_yx[0]) / 2 + radius_offset
+    coords = [np.linspace(- (s - 1) / 2, (s - 1) / 2, s, dtype=data_type) for s in vol_shape]
+    coords = np.meshgrid(*coords, indexing='ij')
+
+    if coords_ball is None:
+        coords_ball = np.arange(-np.fmin(2, len(vol_shape)), 0, dtype=np.int)
+    else:
+        coords_ball = np.array(coords_ball, dtype=np.int)
+
+    radius = np.min(vol_shape[coords_ball]) / 2 + radius_offset
+
+    coords = np.stack(coords, axis=0)
+    if coords_ball.size == 1:
+        dists = np.abs(coords[coords_ball, ...])
+    else:
+        dists = np.sqrt(np.sum(coords[coords_ball, ...] ** 2, axis=0))
+
     if mask_drop_off.lower() == 'const':
-        return np.sqrt(xx ** 2 + yy ** 2) <= radius
+        return dists <= radius
     elif mask_drop_off.lower() == 'sinc':
-        outter_region = 1 - (np.sqrt(xx ** 2 + yy ** 2) <= radius)
-        outter_vals = 1 - np.sinc((np.sqrt(xx ** 2 + yy ** 2) - radius) / ((vol_shape_yx[0]) / np.sqrt(2) - radius))
-        return 1 - outter_region * outter_vals
+        cut_off = np.min(vol_shape[coords_ball]) / np.sqrt(2) - radius
+        outter_region = 1 - (dists <= radius)
+        outter_vals = 1 - np.sinc((dists - radius) / cut_off)
+        return np.fmax(1 - outter_region * outter_vals, 0)
     else:
         raise ValueError('Unknown drop-off function: %s' % mask_drop_off)
 
