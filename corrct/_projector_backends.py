806e91cedf7b81b50b0df11adb4cc8f3c7e09cc2
Nicola VIGANÒ <nicola.vigano@esrf.fr>
2021-03-19 12:42:37 +0100

    Updated docstrings
    Signed-off-by: Nicola VIGANÒ <nicola.vigano@esrf.fr>

--- corrct/_projector_backends.py
+++ corrct/_projector_backends.py
@@ -24,6 +24,16 @@ except ImportError:
 class ProjectorBackend(object):
 
     def __init__(self, vol_shape_yxz, angles_rot_rad):
+        """
+        Base projector backend class. All backends should inherit from this class.
+
+        Parameters
+        ----------
+        vol_shape_yxz : tuple, list or np.ndarray
+            The volume shape.
+        angles_rot_rad : tuple, list or np.ndarray
+            The projection angles.
+        """
         self.vol_shape = [*vol_shape_yxz[2:], vol_shape_yxz[0], vol_shape_yxz[1]]
         self.angles_rot_rad = angles_rot_rad
         self.prj_shape = [*vol_shape_yxz[2:], len(self.angles_rot_rad), vol_shape_yxz[1]]
@@ -31,15 +41,41 @@ class ProjectorBackend(object):
         self.is_initialized = False
 
     def get_vol_shape(self):
+        """
+        Returns the expected and produced volume shape (in ZYX coordinates).
+
+        Returns
+        -------
+        tuple
+            The volume shape.
+        """
         return self.vol_shape
 
     def get_prj_shape(self):
+        """
+        Returns the expected and produced projection shape (in VWU coordinates).
+
+        Returns
+        -------
+        tuple
+            The projection shape.
+        """
         return self.prj_shape
 
     def initialize(self):
+        """
+        Initialization of the projector.
+
+        It should make sure that all the resources have been allocated.
+        """
         self.is_initialized = True
 
     def dispose(self):
+        """
+        De-initialization of the projector.
+
+        It should make sure that all the resources have been de-allocated.
+        """
         self.is_initialized = False
 
     def __del__(self):
@@ -47,12 +83,42 @@ class ProjectorBackend(object):
             self.dispose()
 
     def fp(self, vol, angle_ind: int = None):
+        """
+        Forward-projection interface. Derived backends need to implement this method.
+
+        Parameters
+        ----------
+        vol : numpy.array_like
+            The volume to forward-project.
+        angle_ind : int, optional
+            The angle index to foward project. The default is None.
+        """
         raise NotImplementedError("Method FP not implemented.")
 
     def bp(self, prj, angle_ind: int = None):
+        """
+        Back-projection interface. Derived backends need to implement this method.
+
+        Parameters
+        ----------
+        prj : numpy.array_like
+            The sinogram to back-project or a single line.
+        angle_ind : int, optional
+            The angle index to foward project. The default is None.
+        """
         raise NotImplementedError("Method BP not implemented.")
 
     def fbp(self, prj, fbp_filter):
+        """
+        Filtered back-projection interface. Derived backends need to implement this method.
+
+        Parameters
+        ----------
+        prj : numpy.array_like
+            The sinogram or stack of sinograms.
+        fbp_filter : str
+            The filter to use in the filtered back-projection.
+        """
         raise NotImplementedError("Method FBP not implemented.")
 
 
@@ -110,7 +176,7 @@ class ProjectorBackendSKimage(ProjectorBackend):
         else:
             return np.squeeze(skt.radon(vol, self.angles_rot_deg[angle_ind:angle_ind+1:]))
 
-    def bp(self, prj, angle_ind: int = None):
+    def bp(self, prj: np.ndarray, angle_ind: int = None):
         """
         Back-projection of a single sinogram line to the volume.
 
@@ -134,7 +200,22 @@ class ProjectorBackendSKimage(ProjectorBackend):
         else:
             return skt.iradon(prj[:, np.newaxis], self.angles_rot_deg[angle_ind:angle_ind+1:], filter=None)
 
-    def fbp(self, prj, fbp_filter):
+    def fbp(self, prj: np.ndarray, fbp_filter: str):
+        """
+        Filtered back-projection of a sinogram or stack of sinograms
+
+        Parameters
+        ----------
+        prj : numpy.array_like
+            The sinogram or stack of sinograms.
+        fbp_filter : str
+            The filter to use in the filtered back-projection.
+
+        Returns
+        -------
+        vol : numpy.array_like
+            The reconstructed volume.
+        """
         if len(prj.shape) > 2:
             num_lines = prj.shape[1]
             vol = np.empty([num_lines, *self.vol_shape], dtype=prj.dtype)
@@ -242,9 +323,25 @@ class ProjectorBackendASTRA(ProjectorBackend):
             self.proj_geom_all = astra.create_proj_geom("parallel_vec", vol_shape[0], vectors)
 
     def get_vol_shape(self):
+        """
+        Returns the expected and produced volume shape (in ZYX coordinates).
+
+        Returns
+        -------
+        tuple
+            The volume shape.
+        """
         return astra.functions.geom_size(self.vol_geom)
 
     def get_prj_shape(self):
+        """
+        Returns the expected and produced projection shape (in VWU coordinates).
+
+        Returns
+        -------
+        tuple
+            The projection shape.
+        """
         return astra.functions.geom_size(self.proj_geom_all)
 
     def initialize(self):
@@ -270,7 +367,7 @@ class ProjectorBackendASTRA(ProjectorBackend):
         super().initialize()
 
     def dispose(self):
-        """Disposal of the ASTRA projectors."""
+        """De-initialization of the ASTRA projectors."""
         for p in self.proj_id:
             astra.projector.delete(p)
         self.proj_id = []
@@ -333,6 +430,21 @@ class ProjectorBackendASTRA(ProjectorBackend):
             return self.W_ind[angle_ind].BP(sino)
 
     def fbp(self, prj, fbp_filter):
+        """
+        Filtered back-projection of a sinogram or stack of sinograms
+
+        Parameters
+        ----------
+        prj : numpy.array_like
+            The sinogram or stack of sinograms.
+        fbp_filter : str
+            The filter to use in the filtered back-projection.
+
+        Returns
+        -------
+        vol : numpy.array_like
+            The reconstructed volume.
+        """
         self.initialize()
 
         if has_cuda:
