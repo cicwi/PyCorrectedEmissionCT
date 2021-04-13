Examples: fixed usage of variance for weighted least-squares.
Signed-off-by: Nicola Vigano <nicola.vigano@esrf.fr>

examples/example_04_synthetic_phantom_dead-zone_least-squares.py
@@ -47,9 +47,9 @@ vol_mask = corrct.utils_proc.get_circular_mask(ph_or.shape)
 
 sino_substract = sino - background_avg
 
-sino_stddev = np.sqrt(np.abs(sino))
-min_nonzero_stddev = np.min(sino_stddev[sino > 0])
-sino_tolerances = np.fmax(sino_stddev, min_nonzero_stddev)
+sino_variance = np.abs(sino)
+min_nonzero_variance = np.min(sino_variance[sino > 0])
+sino_tolerances = np.fmax(sino_variance, min_nonzero_variance)
 sino_weights = 1 / sino_tolerances
 
 lowlim_l2 = corrct.solvers.Constraint_LowerLimit(0, norm=corrct.solvers.DataFidelity_l2())
@@ -57,7 +57,7 @@ lowlim_l2w = corrct.solvers.Constraint_LowerLimit(0, norm=corrct.solvers.DataFid
 
 data_term_ls = corrct.solvers.DataFidelity_l2()
 data_term_lsw = corrct.solvers.DataFidelity_wl2(sino_weights)
-data_term_lsb = corrct.solvers.DataFidelity_l2b(sino_tolerances)
+data_term_lsb = corrct.solvers.DataFidelity_l2b(sino_variance)
 
 with corrct.projectors.ProjectorUncorrected(ph.shape, angles) as A:
     solver_ls = corrct.solvers.CP(verbose=True, data_term=data_term_ls)

examples/example_05_synthetic_phantom_basic_regularization.py
@@ -42,15 +42,15 @@ bckgnd_weight = np.sqrt(background_avg / (vol_shape[0] * np.sqrt(2)))
 
 prec = True
 num_iterations = 200
-reg_weight = 1/4
+reg_weight = 1/5
 lower_limit = None
 vol_mask = corrct.utils_proc.get_circular_mask(ph_or.shape)
 
 sino_substract = sino - background_avg
 
-sino_stddev = np.sqrt(np.abs(sino))
-min_nonzero_stddev = np.min(sino_stddev[sino > 0])
-sino_tolerances = np.fmax(sino_stddev, min_nonzero_stddev)
+sino_variance = np.abs(sino)
+min_nonzero_variance = np.min(sino_variance[sino > 0])
+sino_tolerances = np.fmax(sino_variance, min_nonzero_variance)
 sino_weights = 1 / sino_tolerances
 
 reg_tv = corrct.solvers.Regularizer_TV2D(reg_weight)
@@ -77,9 +77,9 @@ data_term_kl = corrct.solvers.DataFidelity_KL()
 data_term_kl_bck = corrct.solvers.DataFidelity_KL(background=background_avg)
 
 data_term_lsw = corrct.solvers.DataFidelity_wl2(sino_weights)
-data_term_lsb = corrct.solvers.DataFidelity_l2b(sino_tolerances)
-data_term_l1b = corrct.solvers.DataFidelity_l1b(sino_tolerances)
-data_term_hub = corrct.solvers.DataFidelity_Huber(sino_tolerances)
+data_term_lsb = corrct.solvers.DataFidelity_l2b(sino_variance)
+data_term_l1b = corrct.solvers.DataFidelity_l1b(sino_variance)
+data_term_hub = corrct.solvers.DataFidelity_Huber(sino_variance)
 
 solver_1 = corrct.solvers.CP(
     verbose=True, data_term=data_term_ls, regularizer=reg_1, tolerance=0, data_term_test=data_term_lsw)