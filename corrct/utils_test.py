c071b3460a63b5ae501fdd7c0e4567868a60f0f9
Nicola VIGANO <nicola.vigano@esrf.fr>
2021-11-15 16:23:57 +0100

    Tests: added type hinting
    Signed-off-by: Nicola VIGANO <nicola.vigano@esrf.fr>

--- corrct/utils_test.py
+++ corrct/utils_test.py
@@ -14,7 +14,8 @@ from . import projectors
 
 from dataclasses import dataclass
 
-from typing import Union, Sequence, Optional
+from typing import Union, Sequence, Optional, Tuple
+from numpy.typing import ArrayLike, DTypeLike
 
 import copy as cp
 
@@ -30,21 +31,21 @@ except ImportError as exc:
     __has_xraylib__ = False
 
 
-def roundup_to_pow2(x, p: int, data_type=np.intp):
+def roundup_to_pow2(x: Union[int, float, ArrayLike], p: int, data_type: DTypeLike = int) -> Union[int, float, ArrayLike]:
     """Round first argument to the power of 2 indicated by second argument.
 
     Parameters
     ----------
-    x : int | float | numpy.array_like
+    x : int | float | ArrayLike
         Number to round up.
     p : int
         Power of 2.
-    data_type : numpy.dtype, optional
+    data_type : DTypeLike, optional
         data type of the output. The default is np.intp.
 
     Returns
     -------
-    int | float | numpy.array_like
+    int | float | ArrayLike
         Rounding up of input.
     """
     return np.ceil(np.array(x) / (2 ** p)).astype(data_type) * (2 ** p)
@@ -80,12 +81,12 @@ class DetectorXRF(object):
     angle_rad: float = (np.pi / 2)
 
     @property
-    def solid_angle_sr(self):
+    def solid_angle_sr(self) -> Union[float, ArrayLike]:
         """Compute the solid angle covered by the detector.
 
         Returns
         -------
-        float | nunmpy.array_like
+        float | ArrayLike
             The computed solid angle of the detector geometry.
         """
         return self.surface_mm2 / (np.pi * self.distance_mm ** 2)
@@ -142,7 +143,7 @@ class Phantom(object):
         else:
             return xraylib.SymbolToAtomicNumber(element)
 
-    def get_attenuation(self, energy_keV: float):
+    def get_attenuation(self, energy_keV: float) -> ArrayLike:
         """Compute the local attenuation for each voxel.
 
         Parameters
@@ -152,7 +153,7 @@ class Phantom(object):
 
         Returns
         -------
-        nunmpy.array_like
+        ArrayLike
             The computed local attenuation volume.
         """
         ph_lin_att = np.zeros(self.shape, self.dtype)
@@ -167,7 +168,7 @@ class Phantom(object):
             ph_lin_att += ph * cmp["density"] * cmp_cs
         return ph_lin_att * self.voxel_size_cm
 
-    def get_element_mass_fraction(self, element: Union[str, int]):
+    def get_element_mass_fraction(self, element: Union[str, int]) -> ArrayLike:
         """Compute the local element mass fraction through out all the phases.
 
         Parameters
@@ -177,7 +178,7 @@ class Phantom(object):
 
         Returns
         -------
-        mass_fraction : nunmpy.array_like
+        mass_fraction : ArrayLike
             The local mass fraction in each voxel.
         """
         el_num = self.get_element_number(element)
@@ -189,7 +190,7 @@ class Phantom(object):
                 mass_fraction += ph * cmp["density"] * cmp["massFractions"][el_ind]
         return mass_fraction
 
-    def _check_parallax_detector(self, detector: DetectorXRF, tolerance: float = 1e-2):
+    def _check_parallax_detector(self, detector: DetectorXRF, tolerance: float = 1e-2) -> bool:
         half_sample_size = np.max(self.voxel_size_cm * self.shape) / 2
 
         if isinstance(detector.distance_mm, float) or (
@@ -206,7 +207,7 @@ class Phantom(object):
 
     def get_compton_scattering(
         self, energy_in_keV: float, angle_rad: Optional[float] = None, detector: Optional[DetectorXRF] = None
-    ):
+    ) -> Tuple[float, ArrayLike]:
         """Compute the local Compton scattering.
 
         Parameters
@@ -227,7 +228,7 @@ class Phantom(object):
         -------
         energy_out_keV : float
             The energy of the Compton radiation received by the detector.
-        cmptn_yield : nunmpy.array_like
+        cmptn_yield : ArrayLike
             Local yield of Compton radiation.
 
         Either `angle_rad` or `detector` need to be supplied.
@@ -263,7 +264,7 @@ class Phantom(object):
 
     def get_fluo_yield(
         self, element: Union[str, int], energy_in_keV: float, fluo_line: int, detector: Optional[DetectorXRF] = None
-    ):
+    ) -> Tuple[float, ArrayLike]:
         """Compute the local fluorescence yield, for the given line of the given element.
 
         Parameters
@@ -281,7 +282,7 @@ class Phantom(object):
         -------
         energy_out_keV : float
             The emitted fluorescence energy.
-        el_yield : nunmpy.array_like
+        el_yield : ArrayLike
             The local fluorescence yield in each voxel.
         """
         if detector:
@@ -300,7 +301,9 @@ class Phantom(object):
         return (energy_out_keV, el_yield)
 
 
-def phantom_assign_concentration(ph_or, element: str = "Ca", in_energy_keV: float = 20.0):
+def phantom_assign_concentration(
+    ph_or: ArrayLike, element: str = "Ca", in_energy_keV: float = 20.0
+) -> Tuple[ArrayLike, ArrayLike, ArrayLike]:
     """Build an XRF phantom.
 
     The created phantom has been used in:
@@ -310,7 +313,7 @@ def phantom_assign_concentration(ph_or, element: str = "Ca", in_energy_keV: floa
 
     Parameters
     ----------
-    ph_or : numpy.array_like
+    ph_or : ArrayLike
         The phases phantom map.
     element : str, optional
         Element symbol. The default is "Ca".
@@ -319,11 +322,11 @@ def phantom_assign_concentration(ph_or, element: str = "Ca", in_energy_keV: floa
 
     Returns
     -------
-    vol_fluo_yield : numpy.array_like
+    vol_fluo_yield : ArrayLike
         Voxel-wise fluorescence yields.
-    vol_att_in : numpy.array_like
+    vol_att_in : ArrayLike
         Voxel-wise attenuation at the incoming beam energy.
-    vol_att_out : numpy.array_like
+    vol_att_out : ArrayLike
         Voxel-wise attenuation at the emitted energy.
     """
     ph_air = ph_or < 0.1
@@ -351,27 +354,27 @@ def phantom_assign_concentration(ph_or, element: str = "Ca", in_energy_keV: floa
 
 
 def create_sino(
-    ph,
+    ph: ArrayLike,
     num_angles: int,
     start_angle_deg: float = 0,
     end_angle_deg: float = 180,
     dwell_time_s: float = 1,
     photon_flux: float = 1e9,
     detectors_pos_rad: Union[float, Sequence] = (np.pi / 2),
-    vol_att_in=None,
-    vol_att_out=None,
-    psf=None,
+    vol_att_in: Optional[ArrayLike] = None,
+    vol_att_out: Optional[ArrayLike] = None,
+    psf: Optional[ArrayLike] = None,
     background_avg: Optional[float] = None,
     background_std: Optional[float] = None,
     add_poisson: bool = False,
     readout_noise_std: Optional[float] = None,
-    data_type=np.float32,
-) -> tuple:
+    data_type: DTypeLike = np.float32,
+) -> Tuple[ArrayLike, ArrayLike, ArrayLike]:
     """Compute the sinogram from a given phantom.
 
     Parameters
     ----------
-    ph : numpy.array_like
+    ph : ArrayLike
         The phantom volume, with the expected average photon production per voxel per impinging photon.
     num_angles : int
         The number of angles.
@@ -383,13 +386,13 @@ def create_sino(
         The acquisition time per sinogram point. The default is 1.
     photon_flux : float, optional
         The impinging photon flux per unit time (second). The default is 1e9.
-    detectors_pos_rad : float | tuple | list | numpy.array_like, optional
+    detectors_pos_rad : float | tuple | list | ArrayLike, optional
         Detector(s) positions in radians, with respect to incoming beam. The default is (np.pi / 2).
-    vol_att_in : numpy.array_like, optional
+    vol_att_in : ArrayLike, optional
         Attenuation volume for the incoming beam. The default is None.
-    vol_att_out : numpy.array_like, optional
+    vol_att_out : ArrayLike, optional
         Attenuation volume for the outgoing beam. The default is None.
-    psf : numpy.array_like, optional
+    psf : ArrayLike, optional
         Point spread function or probing beam profile. The default is None.
     background_avg : float, optional
         Background average value. The default is None.
@@ -404,7 +407,7 @@ def create_sino(
 
     Returns
     -------
-    tuple(numpy.array_like, numpy.array_like, numpy.array_like)
+    Tuple[ArrayLike, ArrayLike, ArrayLike]
         The sinogram (detector readings), the angular positions, and the expected average phton production per voxel.
     """
     print("Creating Sino with %d angles" % num_angles)
@@ -443,21 +446,22 @@ def create_sino(
     return (sino_noise, angles_rad, ph * num_photons * detector_solidangle_sr, background)
 
 
-def compute_error_power(expected_vol, computed_vol) -> tuple:
+def compute_error_power(expected_vol: ArrayLike, computed_vol: ArrayLike) -> Tuple[float, float]:
     """Compute the expected volume signal power, and computed volume error power.
 
     Parameters
     ----------
-    expected_vol : numpy.array_like
+    expected_vol : ArrayLike
         The expected volume.
-    computed_vol : numpy.array_like
+    computed_vol : ArrayLike
         The computed volume.
 
     Returns
     -------
-    tuple(float, float)
+    Tuple[float, float]
         The expected volume signal power, and the computed volume.
     """
     vol_power = np.sqrt(np.sum((expected_vol) ** 2) / expected_vol.size)
     error_power = np.sqrt(np.sum(np.abs(expected_vol - computed_vol) ** 2) / expected_vol.size)
-    return (vol_power, error_power)
+    return vol_power, error_power
+
