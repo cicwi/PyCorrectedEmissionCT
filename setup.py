490fc4972db130621d237f6af4acf3c14b35ebd5
Nicola VIGANO <nicola.vigano@esrf.fr>
2021-11-15 16:54:04 +0100

    Python: bumped dependencies to numpy 1.20 and Python 3.7
    Signed-off-by: Nicola VIGANO <nicola.vigano@esrf.fr>

--- setup.py
+++ setup.py
@@ -6,76 +6,76 @@
 from setuptools import setup, find_packages
 import os.path
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('CHANGELOG.md') as history_file:
+with open("CHANGELOG.md") as history_file:
     history = history_file.read()
 
-with open(os.path.join('corrct', 'VERSION')) as version_file:
+with open(os.path.join("corrct", "VERSION")) as version_file:
     version = version_file.read().strip()
 
 requirements = [
-    'scikit-image',
-    'tqdm',
-    'scipy',
-    'numpy',
-    'dataclasses'
+    "scikit-image",
+    "tqdm",
+    "scipy",
+    "numpy>=1.20",
+    "dataclasses",
 ]
 
 setup_requirements = []
 
 test_requirements = [
-    'matplotlib',
+    "matplotlib",
 ]
 
 dev_requirements = [
-    'autopep8',
-    'rope',
-    'jedi',
-    'flake8',
-    'importmagic',
-    'autopep8',
-    'black',
-    'yapf',
-    'snakeviz',
+    "autopep8",
+    "rope",
+    "jedi",
+    "flake8",
+    "importmagic",
+    "autopep8",
+    "black",
+    "yapf",
+    "snakeviz",
     # Documentation
-    'sphinx',
-    'sphinx_rtd_theme',
-    'recommonmark',
+    "sphinx",
+    "sphinx_rtd_theme",
+    "recommonmark",
     # Other
-    'watchdog',
-    'coverage',
+    "watchdog",
+    "coverage",
 ]
 
 setup(
     author="Nicola VIGANÒ",
-    author_email='N.R.Vigano@cwi.nl',
+    author_email="N.R.Vigano@cwi.nl",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="Physically corrected projectors for X-ray induced emission CT.",
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
-    long_description_content_type='text/markdown',
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords='corrct',
-    name='corrct',
-    packages=find_packages(include=['corrct']),
-    python_requires='>=3.5',
+    keywords="corrct",
+    name="corrct",
+    packages=find_packages(include=["corrct"]),
+    python_requires=">=3.7",
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    extras_require={'dev': dev_requirements},
-    url='https://github.com/cicwi/PyCorrectedEmissionCT',
+    extras_require={"dev": dev_requirements},
+    url="https://github.com/cicwi/PyCorrectedEmissionCT",
     version=version,
     zip_safe=False,
 )
