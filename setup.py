928a80cbddc82befc9178df94a66602acfb2b156
Paddy Harrison <pj.harrison31@gmail.com>
2023-04-29 13:25:03 +0200

    update packaging in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 """The setup script."""
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
-if __name__ == '__main__':
-    setup()
+if __name__ == "__main__":
+    setup(packages=find_packages("./corrct"), package_dir={"": "./corrct"})
