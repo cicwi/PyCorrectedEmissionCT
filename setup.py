6bb5aa235a9f91361b563ffded7c4c3dd14db44e
Nicola VIGANO <nicola.vigano@esrf.fr>
2023-05-16 15:51:57 +0200

    Package-pip: moved package sources finding to setup.cfg
    Signed-off-by: Nicola VIGANO <nicola.vigano@esrf.fr>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 """The setup script."""
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 if __name__ == "__main__":
-    setup(packages=find_packages(include=["corrct", "corrct.*"]))
+    setup()
