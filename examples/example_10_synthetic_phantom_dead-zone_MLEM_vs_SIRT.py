df595561d2e5d8844fd14922ff048a1ac2675712
Jerome Lesaint <jerome.lesaint@esrf.fr>
2023-07-04 19:00:53 +0200

    Clean example 10

--- examples/example_10_synthetic_phantom_dead-zone_MLEM_vs_SIRT.py
+++ examples/example_10_synthetic_phantom_dead-zone_MLEM_vs_SIRT.py
@@ -54,22 +54,12 @@ ph_or = ph_or[:, :, 1]
 
 bckgnd_weight = np.sqrt(background_avg / (vol_shape[0] * np.sqrt(2)))
 
-num_iterations = 200
+num_iterations_sirt = 200
+num_iterations_mlem = 50
 lower_limit = 0
 vol_mask = cct.processing.circular_mask(ph_or.shape)
 
 sino_substract = sino - background_avg
-
-# Extend detector to prevent numerical issues
-fov_extension_factor = 0.05
-det_shape_from_vol = sino_substract.shape[1]
-nb_additional_pixels = int(det_shape_from_vol * fov_extension_factor)
-new_det_shape = det_shape_from_vol + 2* nb_additional_pixels
-extended_sino_substract = np.zeros([sino_substract.shape[0],new_det_shape],dtype=np.float32)
-extended_sino_substract[:,nb_additional_pixels:nb_additional_pixels + det_shape_from_vol] = sino_substract
-
-
-
 
 sino_variance = cct.processing.compute_variance_poisson(sino)
 sino_weights = cct.processing.compute_variance_weight(sino_variance)
@@ -85,14 +75,10 @@ data_term_lsb = cct.solvers.DataFidelity_l2b(sino_variance)
 with cct.projectors.ProjectorUncorrected(ph.shape, angles) as A:
     
     solver_sirt = cct.solvers.SIRT(verbose=True)
-    rec_sirt, _ = solver_sirt(A, sino_substract, num_iterations, x_mask=vol_mask)
+    rec_sirt, _ = solver_sirt(A, sino_substract, num_iterations_sirt, x_mask=vol_mask)
 
     solver_mlem = cct.solvers.MLEM(verbose=True, data_term=data_term_ls)
-    prj_geom = cct.models.ProjectionGeometry.get_default_parallel(geom_type="2d")
-    det_shape_from_vol = A.projector_backend.vol_geom.shape_xyz[0]
-    prj_geom.det_shape_vu = np.array(list([new_det_shape]), dtype=int)
-    A.projector_backend.initialize_geometry(A.projector_backend.vol_geom,angles,prj_geom=prj_geom)
-    rec_mlem, _ = solver_mlem(A, extended_sino_substract, num_iterations, x_mask=vol_mask)
+    rec_mlem, _ = solver_mlem(A, sino_substract, num_iterations_mlem, x_mask=vol_mask, lower_limit=1e-5, upper_limit=10)
 
 # Reconstructions
 f = plt.figure(figsize=cm2inch([24, 24]))
@@ -123,7 +109,6 @@ im_sirt = ax_sirt.imshow(np.squeeze(rec_sirt),vmin=0.,vmax=3.)
 ax_sirt.set_title(solver_sirt.info().upper())
 f.colorbar(im_sirt, ax=ax_sirt)
 
-
 ax_mlem = f.add_subplot(gs[4:, 1], sharex=ax_ph, sharey=ax_ph)
 im_mlem = ax_mlem.imshow(np.squeeze(rec_mlem),vmin=0.,vmax=3.)
 ax_mlem.set_title(solver_mlem.info().upper())
@@ -134,24 +119,16 @@ f.tight_layout()
 
 
 
-f, axs = plt.subplots(1, 2, sharex=True, sharey=True)
-axs[0].plot(np.squeeze(expected_ph[..., 172]), label="Phantom")
-axs[0].plot(np.squeeze(rec_ls[..., 172]), label=solver_ls.info().upper())
-axs[0].plot(np.squeeze(rec_wls[..., 172]), label=solver_wls.info().upper())
-axs[0].plot(np.squeeze(rec_lsb[..., 172]), label=solver_lsb.info().upper())
-axs[0].legend()
-axs[0].grid()
-axs[1].plot(np.squeeze(expected_ph[..., 172]), label="Phantom")
-axs[1].plot(np.squeeze(rec_ls_l[..., 172]), label=solver_ls_l.info().upper())
-axs[1].plot(np.squeeze(rec_wls_l[..., 172]), label=solver_wls_l.info().upper())
-axs[1].plot(np.squeeze(rec_lsb_l[..., 172]), label=solver_lsb_l.info().upper())
-axs[1].legend()
-axs[1].grid()
+f,ax = plt.subplots()
+ax.plot(np.squeeze(expected_ph[..., 172]), label="Phantom")
+ax.plot(np.squeeze(rec_sirt[..., 172]), label=solver_sirt.info().upper())
+ax.plot(np.squeeze(rec_mlem[..., 172]), label=solver_mlem.info().upper())
+ax.legend()
 f.tight_layout()
 
-# Comparing FRCs for each reconstruction
-labels = [solver_ls.info().upper(), solver_wls.info().upper(), solver_lsb.info().upper()]
-vols = [rec_ls, rec_wls, rec_lsb]
-cct.processing.post.plot_frcs([(expected_ph, rec) for rec in vols], labels=labels, snrt=0.4142)
-
-plt.show(block=False)
+## Comparing FRCs for each reconstruction
+#labels = [solver_ls.info().upper(), solver_wls.info().upper(), solver_lsb.info().upper()]
+#vols = [rec_ls, rec_wls, rec_lsb]
+#cct.processing.post.plot_frcs([(expected_ph, rec) for rec in vols], labels=labels, snrt=0.4142)
+#
+#plt.show(block=False)
