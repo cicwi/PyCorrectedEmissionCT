Physics: changed fluo lines API and fixed usage of xraylib for XRF production calculation
Signed-off-by: Nicola VIGANÒ <nicola.vigano@cea.fr>

src/corrct/physics/attenuation.py
@@ -191,7 +191,7 @@ class AttenuationVolume:
         slice_shape = self.vol_shape_zyx[list(axes)]
         coords = [(-(s - 1) / 2, (s - 1) / 2) for s in slice_shape]
 
-        extent = list(np.concatenate(coords))
+        extent = tuple(np.concatenate(coords))
         ax.imshow(att_map, extent=extent)
 
         if other_dim == -3:
@@ -396,7 +396,7 @@ def plot_emission_line_attenuation(
     print(f"Expected intensity: {I_lin}, measured: {I_meas} ({I_meas / I_lin:%})")
     print(f"Mean energy {nrgs_keV.dot(yg / np.sum(yg) * (atts / atts[len(atts) // 2]))}, {nrgs_keV.dot(yg / np.sum(yg))}")
 
-    
+
 def plot_transmittance_decay(
     compounds: Union[str, dict, Sequence[Union[str, dict]]],
     mean_energy_keV: float,
@@ -438,4 +438,3 @@ def plot_transmittance_decay(
     axs.set_title(f"Transmittance curve at {mean_energy_keV:.2f} keV", fontsize=14)
     fig.tight_layout()
     plt.plot(block=False)
-

src/corrct/physics/materials.py
@@ -21,7 +21,7 @@ from corrct.physics.xraylib_helper import xraylib
 
 class VolumeMaterial:
     """
-    VolumeMaterial class, that can be used for predicting fluorescence and Compton yields, attenuation, etc.
+    VolumeMaterial class, that can be used for predicting fluorescence and Compton productions, attenuation, etc.
 
     Parameters
     ----------
@@ -165,8 +165,8 @@ class VolumeMaterial:
         -------
         energy_out_keV : float
             The energy of the Compton radiation received by the detector.
-        cmptn_yield : NDArray
-            Local yield of Compton radiation.
+        cmptn_prod : NDArray
+            Local production of Compton radiation.
 
         Either `angle_rad` or `detector` need to be supplied.
         """
@@ -179,7 +179,7 @@ class VolumeMaterial:
             if angle_rad is None:
                 angle_rad = detector.angle_rad
 
-        cmptn_yield = np.zeros(self.shape, self.dtype)
+        cmptn_prod = np.zeros(self.shape, self.dtype)
         for ph, cmp in zip(self.materials_fractions, self.materials_compositions):
             try:
                 cmp_cs = xraylib.DCS_Compt_CP(cmp["name"], energy_in_keV, angle_rad)
@@ -199,30 +199,34 @@ class VolumeMaterial:
                     + ", pixel-size {self.voxel_size_cm}"
                     + f" - total {cmp['density'] * cmp_cs * self.voxel_size_cm} (assuming material mass fraction = 1)"
                 )
-            cmptn_yield += ph * cmp["density"] * cmp_cs
-        cmptn_yield *= self.voxel_size_cm
+            cmptn_prod += ph * cmp["density"] * cmp_cs
+        cmptn_prod *= self.voxel_size_cm
 
         if detector:
-            cmptn_yield *= detector.solid_angle_sr
+            cmptn_prod *= detector.solid_angle_sr
 
         energy_out_keV = xraylib.ComptonEnergy(energy_in_keV, angle_rad)
-        return (energy_out_keV, cmptn_yield)
+        return (energy_out_keV, cmptn_prod)
 
-    def get_fluo_yield(
+    def get_fluo_production(
         self,
         element: Union[str, int],
         energy_in_keV: float,
         fluo_lines: Union[str, xrf.FluoLine, Sequence[xrf.FluoLine]],
         detector: Union[xrf.DetectorXRF, None] = None,
     ) -> tuple[float, NDArray]:
-        """Compute the local fluorescence yield, for the given line of the given element.
+        """Compute the local fluorescence production, for the given line of the given element.
+
+        Using Eq. (1) from:
+        - T. Schoonjans et al., "The xraylib library for X-ray-matter interactions. Recent developments," Spectrochim. Acta
+          Part B At. Spectrosc., vol. 66, no. 11-12, pp. 776-784, Nov. 2011, doi: 10.1016/j.sab.2011.09.011.
 
         Parameters
         ----------
         element : str | int
             The element to consider.
         energy_in_keV : float
-            The incombing X-ray beam energy.
+            The incoming X-ray beam energy.
         fluo_lines : str | FluoLine | Sequence[FluoLine]
             The fluorescence line to consider.
         detector : DetectorXRF, optional
@@ -232,8 +236,8 @@ class VolumeMaterial:
         -------
         energy_out_keV : float
             The emitted fluorescence energy.
-        el_yield : NDArray
-            The local fluorescence yield in each voxel.
+        el_prod : NDArray
+            The local fluorescence production in each voxel.
         """
         if detector:
             if not self._check_parallax_detector(detector):
@@ -246,20 +250,20 @@ class VolumeMaterial:
 
         el_num = get_element_number(element)
 
-        el_cs = np.empty((len(fluo_lines),), self.dtype)
+        el_yield = np.empty((len(fluo_lines),), self.dtype)
         for ii, line in enumerate(fluo_lines):
             try:
-                el_cs[ii] = xraylib.CS_FluorLine_Kissel(el_num, line.indx, energy_in_keV)  # fluo production for cm2/g
+                el_yield[ii] = xraylib.CS_FluorLine_Kissel(el_num, line.indx, energy_in_keV)  # fluo production for cm2/g
             except ValueError as exc:
                 el_sym = xraylib.AtomicNumberToSymbol(el_num)
                 if self.verbose:
                     print(f"Energy {exc}: el_num={el_num} ({el_sym}) line={line}")
-                el_cs[ii] = 0
-        el_yield = self.get_element_mass_fraction(el_num) * np.sum(el_cs) * self.voxel_size_cm
+                el_yield[ii] = 0
+        el_prod = self.get_element_mass_fraction(el_num) * np.sum(el_yield) * self.voxel_size_cm
 
         if detector:
-            el_yield *= detector.solid_angle_sr
+            el_prod *= detector.solid_angle_sr
 
-        energy_out_keV = xrf.LinesSiegbahn.get_energy(el_num, fluo_lines, compute_average=True)
+        energy_out_keV = xrf.get_energy(el_num, fluo_lines, compute_average=True)
 
-        return float(energy_out_keV), el_yield
+        return float(energy_out_keV), el_prod

src/corrct/physics/xrf.py
@@ -94,66 +94,100 @@ class LinesSiegbahn:
         """
         return [f for f in LinesSiegbahn.lines if f.name[: len(line)] == line.upper()]
 
-    @staticmethod
-    def get_energy(
-        element: Union[str, int],
-        lines: Union[str, FluoLine, Sequence[FluoLine]],
-        compute_average: bool = False,
-        verbose: bool = False,
-    ) -> Union[float, NDArray]:
-        """
-        Return the energy(ies) of the requested line for the given element.
 
-        Parameters
-        ----------
-        element : Union[str, int]
-            The requested element.
-        line : str
-            The requested line. It can be a whole shell (transition to that shell),
-            or sub-shells.
-        compute_average : bool, optional
-            Weighted averaging the lines, using the radiation rate. The default is False.
+def _get_lines_list(lines) -> Sequence[FluoLine]:
+    if isinstance(lines, FluoLine):
+        return [lines]
+    elif isinstance(lines, str):
+        return LinesSiegbahn.get_lines(lines)
+    elif len(lines) == 0:
+        raise ValueError(f"No line was passed! lines={lines}")
+    else:
+        return lines
 
-        Returns
-        -------
-        energy_keV : Union[float, NDArray]
-            Either the average energy or the list of different energies.
-        """
-        el_sym, el_num = xraylib_helper.get_element_number_and_symbol(element)
 
-        if isinstance(lines, FluoLine):
-            lines_list = [lines]
-        elif isinstance(lines, str):
-            lines_list = LinesSiegbahn.get_lines(lines)
-        elif len(lines) == 0:
-            raise ValueError(f"No line was passed! lines={lines}")
-        else:
-            lines_list = lines
+def get_radiation_rate(
+    element: Union[str, int],
+    lines: Union[str, FluoLine, Sequence[FluoLine]],
+    verbose: bool = False,
+) -> NDArray:
+    """Return the radiation rates of the requested lines for the given element.
 
-        energy_keV = np.empty(len(lines_list), dtype=np.float32)
-        for ii, line in enumerate(lines_list):
-            try:
-                energy_keV[ii] = xraylib.LineEnergy(el_num, line.indx)
-            except ValueError as exc:
-                if verbose:
-                    print(f"INFO - Energy - {exc}: el_num={el_num} ({el_sym}) line={line}")
-                energy_keV[ii] = 0
+    Parameters
+    ----------
+    element : Union[str, int]
+        The requested element
+    lines : Union[str, FluoLine, Sequence[FluoLine]]
+        The requested line. It can be a whole shell (transition to that shell),
+        or sub-shells.
+    verbose : bool, optional
+        Whether to produce verbose output in case of errors, by default False
 
-        if compute_average:
-            rates = np.empty(energy_keV.shape)
-            for ii, line in enumerate(lines_list):
-                try:
-                    rates[ii] = xraylib.RadRate(el_num, line.indx)
-                except ValueError as exc:
-                    if verbose:
-                        print(f"INFO - RadRate - {exc}: el_num={el_num} ({el_sym}) line={line}")
-                    rates[ii] = 0
-            energy_keV = float(np.sum(energy_keV * rates / np.sum(rates)))
+    Returns
+    -------
+    NDArray
+        The list of radiation rates
+    """
+    el_sym, el_num = xraylib_helper.get_element_number_and_symbol(element)
 
-        if verbose:
-            print(f"{el_sym}-{lines} emission energy (keV):", energy_keV, "\n")
+    lines_list = _get_lines_list(lines)
 
-        return energy_keV
+    rates = np.empty(len(lines_list), dtype=np.float32)
+    for ii, line in enumerate(lines_list):
+        try:
+            rates[ii] = xraylib.RadRate(el_num, line.indx)
+        except ValueError as exc:
+            if verbose:
+                print(f"INFO - RadRate - {exc}: el_num={el_num} ({el_sym}) line={line}")
+            rates[ii] = 0
+    return rates
+
+
+def get_energy(
+    element: Union[str, int],
+    lines: Union[str, FluoLine, Sequence[FluoLine]],
+    compute_average: bool = False,
+    verbose: bool = False,
+) -> Union[float, NDArray]:
+    """
+    Return the energy(ies) of the requested line for the given element.
+
+    Parameters
+    ----------
+    element : Union[str, int]
+        The requested element.
+    line : str
+        The requested line. It can be a whole shell (transition to that shell),
+        or sub-shells.
+    compute_average : bool, optional
+        Weighted averaging the lines, using the radiation rate. The default is False.
+
+    Returns
+    -------
+    energy_keV : Union[float, NDArray]
+        Either the average energy or the list of different energies.
+    """
+    el_sym, el_num = xraylib_helper.get_element_number_and_symbol(element)
+
+    lines_list = _get_lines_list(lines)
+
+    energy_keV = np.empty(len(lines_list), dtype=np.float32)
+    for ii, line in enumerate(lines_list):
+        try:
+            energy_keV[ii] = xraylib.LineEnergy(el_num, line.indx)
+        except ValueError as exc:
+            if verbose:
+                print(f"INFO - Energy - {exc}: el_num={el_num} ({el_sym}) line={line}")
+            energy_keV[ii] = 0
+
+    if compute_average:
+        rates = get_radiation_rate(element, lines_list)
+        energy_keV = float(np.sum(energy_keV * rates / np.sum(rates)))
+
+    if verbose:
+        print(f"{el_sym}-{lines} emission energy (keV):", energy_keV, "\n")
+
+    return energy_keV
 
 
 @dataclass
@@ -173,4 +207,4 @@ class DetectorXRF:
         float | NDArray
             The computed solid angle of the detector geometry.
         """
-        return self.surface_mm2 / (np.pi * self.distance_mm**2)
+        return self.surface_mm2 / (4 * np.pi * self.distance_mm**2)