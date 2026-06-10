70ca9a3b6a5569efa85fd8e807f2a7ea89473fca
Nicola Vigano <nicola.vigano@cea.fr>
2026-06-10 13:59:27 +0200

    Fixed example script
    Signed-off-by: Nicola Vigano <nicola.vigano@cea.fr>

--- examples/alignment/example_02_cone-beam_calibration.py
+++ examples/alignment/example_02_cone-beam_calibration.py
@@ -10,7 +10,9 @@ import h5py
 import numpy as np
 from numpy.typing import NDArray
 
-import corrct as cct
+from corrct.alignment.cone_beam import FitConeBeamGeometry, tune_acquisition_geometry
+from corrct.alignment.markers import create_marker_disk, track_marker
+from corrct.models import plot_projection_geometry
 
 
 def _get_data(fid: h5py.File, data_path: str) -> NDArray:
@@ -26,34 +28,35 @@ def _load_data(fname: str | Path) -> dict[str, NDArray]:
         return {k: _get_data(fid, f"/{k}") for k in fid.keys()}
 
 
-data = _load_data("./data/calibration_scans.h5")
+try:
+    data = _load_data("./data/calibration_scans.h5")
+except FileNotFoundError as exc:
+    raise ValueError("Please download the example dataset from https://doi.org/10.5281/zenodo.20559974") from exc
 
 prj_size_vu = (data["scan_1"].shape[0], data["scan_1"].shape[2])
 
-probe = cct.alignment.markers.create_marker_disk(prj_size_vu, 3.5)
-pos_1, pos_2 = (cct.alignment.markers.track_marker(imgs, probe) for imgs in (data["scan_1"], data["scan_2"]))
+probe = create_marker_disk(prj_size_vu, 3.5)
+pos_l, pos_u = (track_marker(imgs, probe) for imgs in (data["scan_1"], data["scan_2"]))
 
 pixel_size_um = float(data["pixel_size_um"])
 orbit_radius_pix = float(data["orbit_radius_um"]) / pixel_size_um
 
-fit_cb_geom = cct.alignment.cone_beam.FitConeBeamGeometry(
-    prj_size_vu, points_ell1=pos_1, points_ell2=pos_2, pix_size_um=pixel_size_um, plot_result=True
+fit_cb_geom = FitConeBeamGeometry(
+    prj_size_vu, points_ell1=pos_u, points_ell2=pos_l, pix_size_um=pixel_size_um, plot_result=True
 )
 acq_geom = fit_cb_geom.fit(r=orbit_radius_pix)
 
 print(acq_geom)
-cct.models.plot_projection_geometry(acq_geom.get_prj_geom(), acq_geom.get_vol_geom())
 
 imgs_t = (data["scan_1"] + data["scan_2"]).astype(np.float32)
+angles_rot_rad = np.deg2rad(data["angles_deg"])
 
-acq_geom = cct.alignment.cone_beam.tune_acquisition_geometry(
+acq_geom = tune_acquisition_geometry(
     acq_geom,
     data=imgs_t,
-    angles_rot_rad=np.deg2rad(data["angles_deg"]),
+    angles_rot_rad=angles_rot_rad,
     params=dict(
-        D_pix=np.linspace(-24 * 2, 0, 9),
         theta_deg=np.linspace(-1, 1, 5),
-        eta_deg=np.linspace(-1, 1, 5),
         phi_deg=np.linspace(-0.25, 0.25, 5),
         u0_pix=np.linspace(-1, 1, 9),
         v0_pix=np.linspace(-1, 1, 9),
@@ -62,3 +65,4 @@ acq_geom = cct.alignment.cone_beam.tune_acquisition_geometry(
 )
 
 print(acq_geom)
+plot_projection_geometry(acq_geom.get_prj_geom(), acq_geom.get_vol_geom())
