0bb04af8b829b09e1ec32227f461474ec786cf59
Nicola Vigano <nicola.vigano@cea.fr>
2026-07-24 13:34:27 +0200

    Add support for tight Parseval frame based convolutional regularizers
    Signed-off-by: Nicola Vigano <nicola.vigano@cea.fr>

--- src/corrct/operators.py
+++ src/corrct/operators.py
@@ -28,6 +28,14 @@ except ImportError:
     print("WARNING - pywt was not found")
 
 
+try:
+    import torch as pt
+
+    __has_torch__ = True
+except ImportError:
+    __has_torch__ = False
+
+
 NDArrayInt = NDArray[np.integer]
 
 
@@ -494,6 +502,129 @@ class TransformConvolution(BaseTransform):
             return self._crop_valid(x, pw)
         else:
             return x
+
+
+class TransformConvolutionTightFrame(BaseTransform):
+    """
+    Tight Parseval Frame Convolution operator.
+
+    Parameters
+    ----------
+    x_shape : ArrayLike
+        Shape of the direct space.
+    kernel : ArrayLike
+        The convolution kernel.
+    pad_mode: str, optional
+        The padding mode to use for the linear convolution. The default is "edge".
+    """
+
+    kernels: NDArray
+    pad_mode: str
+
+    conv_d: Callable
+    conv_t: Callable
+
+    device: str
+
+    def __init__(self, x_shape: Sequence[int] | NDArray, kernels: NDArray, pad_mode: str = "edge"):
+        if not __has_torch__:
+            raise ValueError(f"PyTorch is not available, thus {self.__class__.__name__} will not be available.")
+
+        self.num_filters = len(kernels)
+        self.dir_shape = np.array(x_shape, ndmin=1, dtype=int)
+        self.adj_shape = np.array([self.num_filters, *x_shape], ndmin=1, dtype=int)
+
+        self.kernels = np.array(kernels, dtype=np.float32)[:, None, ...]
+
+        self.pad_mode = pad_mode.lower()
+
+        if len(x_shape) == 1:
+            self.conv_d = pt.nn.functional.conv1d
+            self.conv_t = pt.nn.functional.conv_transpose1d
+        elif len(x_shape) == 2:
+            self.conv_d = pt.nn.functional.conv2d
+            self.conv_t = pt.nn.functional.conv_transpose2d
+        elif len(x_shape) == 3:
+            self.conv_d = pt.nn.functional.conv3d
+            self.conv_t = pt.nn.functional.conv_transpose3d
+        else:
+            raise ValueError(f"We do not support {len(x_shape)}-dimensional convolutions ({x_shape = })")
+
+        super().__init__()
+
+        self.device = "cuda" if pt.cuda.is_available() else "cpu"
+
+        self.k_t = pt.from_numpy(self.kernels).to(self.device)
+
+        # Check if filters form a tight Parseval frame
+
+        # Check orthonormality
+        kernels_f_matr = kernels.reshape((self.num_filters, -1))
+        gram_matrix = kernels_f_matr @ kernels_f_matr.T
+
+        if not np.allclose(gram_matrix, np.eye(self.num_filters), rtol=1e-5, atol=1e-5):
+            raise ValueError(f"The provided filters are not orthonormal.\n - Gram matrix: {gram_matrix}")
+
+        # We should also check the frequency coverage, but since we might skip the low frequencies sometimes, we would get a
+        # false negative
+        if kernels_f_matr.shape[0] == kernels_f_matr.shape[1]:
+
+            emb = np.zeros(self.adj_shape)
+            slices = [slice(None)] + [slice(s) for s in self.kernels.shape[2:]]
+            emb[tuple(slices)] = self.kernels[:, 0]  # embed (grayscale)
+            axes = tuple([*(range(-self.kernels.ndim + 2, 0))])
+            power_spectrum: NDArray = np.abs(np.fft.fftn(emb, axes=axes)) ** 2
+            fourier_coverage: NDArray = power_spectrum.sum(axis=0)
+
+            if not np.all(np.isclose(fourier_coverage, self.num_filters, rtol=1e-5, atol=1e-5)):
+                raise ValueError("The set of provided filters does not have spectral flatness.")
+
+    def absolute(self) -> "TransformConvolutionTightFrame":
+        """
+        Return the convolution operator using the absolute value of the kernel coefficients.
+
+        Returns
+        -------
+        TransformConvolution
+            The absolute value of the convolution operator.
+        """
+        return TransformConvolutionTightFrame(self.dir_shape, np.abs(self.kernels))
+
+    def _compute_direct_padding(self) -> NDArray:
+        padding = []
+        for s in self.kernels.shape[-len(self.dir_shape) :]:
+            padding.append((s // 2, (s - 1) // 2))
+        return np.array(padding)
+
+    def _compute_adjoint_padding(self) -> tuple[NDArray, NDArray]:
+        pre_padding = [(0, 0)]
+        for s in self.kernels.shape[-len(self.dir_shape) :]:
+            pre_padding.append((s // 2, (s - 1) // 2))
+        post_padding = []
+        for s in self.kernels.shape[-len(self.dir_shape) :]:
+            post_padding.append(((s // 2) * 2, ((s - 1) // 2) * 2))
+        return np.array(pre_padding), np.array(post_padding)
+
+    def _pad_valid(self, x: NDArray, pad_width: NDArray) -> NDArray:
+        return np.pad(x, pad_width=pad_width, mode=self.pad_mode)  # type: ignore
+
+    def _crop_valid(self, x: NDArray, pad_width: NDArray) -> NDArray:
+        slices = [slice(pw[0] if pw[0] else None, -pw[1] if pw[1] else None) for pw in pad_width]
+        return x[tuple(slices)]
+
+    def _op_direct(self, x: NDArray) -> NDArray:
+        pw = self._compute_direct_padding()
+        x = self._pad_valid(x, pw)
+        x_t = pt.from_numpy(x)[None, None, ...].to(self.device)
+        y_t = self.conv_d(x_t, self.k_t)
+        return y_t.cpu().numpy().copy()[0]
+
+    def _op_adjoint(self, x: NDArray) -> NDArray:
+        pw_pre, pw_post = self._compute_adjoint_padding()
+        x = self._pad_valid(x, pw_pre)
+        x_t = pt.from_numpy(x)[None, ...].to(self.device)
+        y_t = self.conv_t(x_t, self.k_t)
+        return self._crop_valid(y_t.cpu().numpy().copy()[0, 0], pw_post)
 
 
 class BaseWaveletTransform(BaseTransform, ABC):
